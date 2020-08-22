a6e31bd59dea052d0b004192da19ba555af3967d
stackoverprof <mail.errbint@gmail.com>
2020-08-22 13:03:33 +0700

    try4 redeploy

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          SandBoxes<a href="https://nextjs.org">by ERRBINT</a>
+          SandBoxes <a href="https://nextjs.org">by ERRBINT</a>
         </h1>
 
         <p className={styles.description}>
